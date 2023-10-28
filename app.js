dc3301cc292a983b568a916df638314741624e4e
dkumylove <dkumylove@gmail.com>
2023-10-29 02:14:15 +0900

    #2.2 변수

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // alert : 경고창띄움
-alert = "hi";
+// const : constant(상수) 바뀌지 않는 값, 계속 유지됨
+// const 변수명 = 값 ;
+// 변수명을 지을떄 공백이 필요하다면 카멜표기법 사용
+
+
+const a = 10;
+const b = 2;
+const myName = "jieun";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
