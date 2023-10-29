d3f7ca09199e269aeb03ab0b66422af5a961cc9c
dkumylove <dkumylove@gmail.com>
2023-10-30 01:24:05 +0900

    #3.5 추가이벤트

--- app.js
+++ app.js
@@ -473,5 +473,53 @@ function handleMouseLeave(){
 
 // 유저가 title을 click할 경우에 자바스크립트가 개발자 대신 실행버튼을 눌러주게함
 title.addEventListener("click", handleTitleClick);  // 클릭할때
+title.onclick = handleTitleClick;
 title.addEventListener("mouseenter", handleMouseEnter);   // 마우스를 올리면
+title.onmouseenter = handleMouseEnter;
 title.addEventListener("mouseleave", handleMouseLeave);   // 마우스를 내리면
+title.onmouseleave = handleMouseLeave;
+
+//////////////////////////////////////////////////////////////////
+// window의 resize 이벤트
+// 브라우저 창 리사이징시마다 콜백 코드가 실행
+// 의도적으로 매 resize마다 코드를 실행해야 하는 경우도 있겠지만, 그렇지 않은 경우라면 이는 매우 비효율적이고 브라우저에 과부하를 줄 수도 있다. 
+
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+    h1.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    h1.innerText = "mouse is here!"
+}
+
+function handleMouseLeave() {
+    h1.innerText = "mouse is gone!"
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("All gooood");
+}
+
+// 유저가 title을 click할 경우에 자바스크립트가 개발자 대신 실행버튼을 눌러주게함
+h1.addEventListener("click", handleTitleClick);  // 클릭할때
+h1.addEventListener("mouseenter", handleMouseEnter);   // 마우스를 올리면
+h1.addEventListener("mouseleave", handleMouseLeave);   // 마우스를 내리면
+
+window.addEventListener("resize", handleWindowResize);   // 브라우저 창의 크기가 바뀌면
+window.addEventListener("copy", handleWindowCopy);    // 복사할떄 
+window.addEventListener("offline", handleWindowOffline);  // 오프라인일떄
+window.addEventListener("online", handleWindowOnline);   // 온라인일떄
--- app1.js
+++ app1.js
@@ -1,22 +1,41 @@
 // querySelector() : element를 CSS방식으로 검색할수 있음
 
-const title = document.querySelector("div.hello:first-child h1");
-
-console.dir(title);
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    title.style.color = "blue";
+    h1.style.color = "blue";
 }
 
-function handleMouseEnter(){
-    title.innerText = "mouse is here!"
+function handleMouseEnter() {
+    h1.innerText = "mouse is here!"
 }
 
-function handleMouseLeave(){
-    title.innerText = "mouse is gone!"
+function handleMouseLeave() {
+    h1.innerText = "mouse is gone!"
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("All gooood");
 }
 
 // 유저가 title을 click할 경우에 자바스크립트가 개발자 대신 실행버튼을 눌러주게함
-title.addEventListener("click", handleTitleClick);  // 클릭할때
-title.addEventListener("mouseenter", handleMouseEnter);   // 마우스를 올리면
-title.addEventListener("mouseleave", handleMouseLeave);   // 마우스를 내리면
+h1.addEventListener("click", handleTitleClick);  // 클릭할때
+h1.addEventListener("mouseenter", handleMouseEnter);   // 마우스를 올리면
+h1.addEventListener("mouseleave", handleMouseLeave);   // 마우스를 내리면
+
+window.addEventListener("resize", handleWindowResize);   // 브라우저 창의 크기가 바뀌면
+window.addEventListener("copy", handleWindowCopy);    // 복사할떄 
+window.addEventListener("offline", handleWindowOffline);  // 오프라인일떄
+window.addEventListener("online", handleWindowOnline);   // 온라인일떄
